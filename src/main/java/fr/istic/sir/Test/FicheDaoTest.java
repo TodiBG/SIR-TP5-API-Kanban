22284ee977848c33301b77c2e700b2f65eec4e73
ange-clarisse <angecla16@gmail.com>
2021-03-03 22:59:43 +0100

    Add tests on UserDaoTest and FicheDaoTest

---
+++ src/main/java/fr/istic/sir/Test/FicheDaoTest.java
@@ -0,0 +1,147 @@
+package fr.istic.sir.Test;
+
+import fr.istic.sir.jpa.dao.FicheDao;
+import fr.istic.sir.jpa.dao.UserDao;
+import fr.istic.sir.jpa.entities.Fiche;
+import fr.istic.sir.jpa.entities.User;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import java.util.Date;
+import java.util.List;
+
+public class FicheDaoTest {
+
+    FicheDao ficheDao;
+    UserDao userDao;
+    @BeforeEach
+    void init(){
+        ficheDao = new FicheDao();
+        userDao = new UserDao() ;
+
+    }
+
+    @Test
+    @DisplayName("Test retrieve a fiche by his id")
+    void getFicheById(){
+        //We are sure that in our database there is a fiche with this id
+        long id = 2;
+
+        //We retrieve this fiche
+        Fiche fiche = ficheDao.findById(id);
+
+        //We test the id match
+        Assertions.assertEquals(fiche.getId(),id);
+
+    }
+    @Test
+    @DisplayName("Test retrieve all fiche ")
+    void getAllFiche(){
+
+        //We retrieve this fiche
+        List<Fiche> Fiche  = ficheDao.findAll();
+
+        //We test the id match
+        Assertions.assertNotNull(Fiche,"il y a des fiches dans la BD");
+
+    }
+
+    @Test
+    @DisplayName("Test create a fiche")
+    void createFiche(){
+        User user2 = new User("Lena", "Mutoni", "lena.mutoni@gmail.com") ;
+        userDao.create(user2);
+
+        Date date = new Date();
+
+        //We instanciate a new fiche
+        Fiche fiche = new Fiche("Production du premier lot",date,user2,45,"Villejean","http://localhost/phpmyadmin/sql.php?server=1&db=kanban&table=Fiche","frontend");
+
+        // We register this new fiche in the database
+        ficheDao.create(fiche);
+
+        //We retrieve the registration of this new fiche
+        Fiche fiche1 = ficheDao.findById(fiche.getId());
+
+        //We test the fiches match
+        Assertions.assertEquals(fiche1,fiche);
+
+    }
+    @Test
+    @DisplayName("Test update methode ")
+    void updateFiche(){
+        //instantiation of a new fiche
+        User user3 = new User("Nella", "Gatoni", "nella.gatoni@gmail.com") ;
+        userDao.create(user3);
+        Date date = new Date();
+
+        //We instanciate a new fiche
+        Fiche fiche = new Fiche("Production du sixieme lot",date,user3,45,"Villejean","http://localhost/phpmyadmin/sql.php?server=1&db=kanban&table=Fiche","frontend");
+
+        // We register this new fiche in the database
+        ficheDao.create(fiche);
+
+        //modification of the user's name
+        fiche.setTemps(50);
+
+        ficheDao.update(fiche);
+
+        //We test the name match
+        Assertions.assertEquals(fiche.getTemps(),50);
+    }
+
+    @Test
+    @DisplayName("Test delete methode ")
+    void deleteFiche(){
+        //instantiation of a new fiche
+        User user4 = new User("Sam", "Gatoni", "Sam.gatoni@gmail.com") ;
+        userDao.create(user4);
+        Date date = new Date();
+
+        //We instanciate a new fiche
+        Fiche fiche = new Fiche("Production du septieme lot",date,user4,45,"Villejean","http://localhost/phpmyadmin/sql.php?server=1&db=kanban&table=Fiche","frontend");
+
+        // We register this new fiche in the database
+        ficheDao.create(fiche);
+
+
+        //deleting this new fiche
+
+        ficheDao.delete(fiche);
+
+        Fiche fiche1 = ficheDao.findById(fiche.getId());
+
+        //We test if the object is null
+        Assertions.assertNull(fiche1);
+
+    }
+    @Test
+    @DisplayName("Test delete methode ")
+    void deleteFicheById(){
+        //instantiation of a new fiche
+        User user5 = new User("Samuela", "Diouf", "Samuela.diouf@gmail.com") ;
+        userDao.create(user5);
+        Date date = new Date();
+
+        //We instanciate a new fiche
+        Fiche fiche = new Fiche("Production du huitieme lot",date,user5,45,"Villejean","http://localhost/phpmyadmin/sql.php?server=1&db=kanban&table=Fiche","frontend");
+
+        // We register this new fiche in the database
+        ficheDao.create(fiche);
+
+
+        //deleting this new fiche
+
+        ficheDao.deleteById(fiche.getId());
+
+        Fiche fiche1 = ficheDao.findById(fiche.getId());
+
+        //We test if the object is null
+        Assertions.assertNull(fiche1);
+
+    }
+
+
+}
--- src/main/java/fr/istic/sir/Test/UserDaoTest.java
+++ src/main/java/fr/istic/sir/Test/UserDaoTest.java
@@ -1,11 +1,14 @@
 package fr.istic.sir.Test;
 
 import fr.istic.sir.jpa.dao.UserDao;
+import fr.istic.sir.jpa.entities.Fiche;
 import fr.istic.sir.jpa.entities.User;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+
+import java.util.List;
 
 public class UserDaoTest {
 
@@ -17,7 +20,7 @@ public class UserDaoTest {
 
 
     @Test
-    @DisplayName("Test create methode ")
+    @DisplayName("Test get methode ")
     void getUserById(){
 
         //We are sure there is an user in the database who owns this email
@@ -30,6 +33,17 @@ public class UserDaoTest {
         Assertions.assertEquals(user.getEmail(), email);
 
     }
+    @Test
+    @DisplayName("Test retrieve all users ")
+    void getAllFiche(){
+
+        //We retrieve this fiche
+        List<User>Users  = userDao.findAll();
+
+        //We test the id match
+        Assertions.assertNotNull(Users,"il y a des fiches dans la BD");
+
+    }
 
 
 
@@ -37,17 +51,81 @@ public class UserDaoTest {
     @DisplayName("Test create methode ")
     void createUser(){
         String email = "bonaventure.gbehe@gmail.com";
+        //We instanciate a new user
         User user1 = new User("Todi Bonaventure", "Gbehe", email) ;
 
+        //We register this new user in the database
         userDao.create(user1);
 
+        //We retrieve the registration of this new user
         User user2 = userDao.findById(email);
 
         User user3 = userDao.findById("gbehe@gmail.com");
 
+        //We test the users match
         Assertions.assertEquals(user2, user1);
 
     }
+    @Test
+    @DisplayName("Test update methode ")
+    void UpdateUser(){
+        //instantiation of a new user
+        User user = new User("Ange ", "Sibomana", "angecla18@gmail.com") ;
+
+        //registration in database
+        userDao.create(user);
+
+        //modification of the user's name
+        user.setPrenom("Ange Clarisse");
+
+        userDao.update(user);
+
+        //We test the name match
+        Assertions.assertEquals(user.getPrenom(),"Ange Clarisse");
+
+    }
+
+    @Test
+    @DisplayName("Test delete methode ")
+    void deleteUser(){
+        String email = "moussa@gmail.com";
+        //instantiation of a new user
+        User user = new User("Moussa ", "Bamba", email) ;
+
+        //registration in database
+        userDao.create(user);
+
+        //deleting this new user
+
+        userDao.delete(user);
+
+        User user2 = userDao.findById(email);
+
+        //we test if the object is null
+        Assertions.assertNull(user2);
+
+    }
+    @Test
+    @DisplayName("Test deleteUserById methode ")
+    void deleteUserById(){
+        String email = "gretta@gmail.com";
+        //instantiation of a new user
+        User user = new User("Gretta ", "Keza", email) ;
+
+        //registration in database
+        userDao.create(user);
+
+        //deleting this new user
+
+        userDao.deleteById(email);
+
+        User user2 = userDao.findById(email);
+
+        //we test if the object is null
+        Assertions.assertNull(user2,"il n'y a pas cet utilisateur dans la BD ");
+
+    }
+
 
 
 }
